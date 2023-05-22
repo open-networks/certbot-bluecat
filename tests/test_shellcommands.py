557064b0434826c9e297b0d4cfa27222c95880f5
Erich Malzer <em@on.at>
2023-05-22 20:24:14 +0200

    fixed verify for tests again

--- tests/test_shellcommands.py
+++ tests/test_shellcommands.py
@@ -50,7 +50,6 @@ def test_certbot_bluecat():
             f' --bluecat-username \'{user}\''
             f' --bluecat-password \'{password}\''
             f' --bluecat-viewid \'{viewid}\''
-            ' --bluecat-verify-ssl False'
             ' --agree-tos'
             f' --email {email}'
             ' --no-eff-email'
